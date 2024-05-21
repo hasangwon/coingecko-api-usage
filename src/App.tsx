8b7f6ef578818b628b4659fe300fe9627369cde6
hasangwon <305243@naver.com>
2024-05-21 16:51:28 +0900

    Update : session 스토리지 캐싱 기능 react-query로 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+// "use client";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Detail from "./pages/Detail";
 import Scrap from "./pages/Scrap";
 import Home from "./pages/Home";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
-      <ToastContainer />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/scrap" element={<Scrap />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/scrap" element={<Scrap />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
--- src/hooks/useFetchCoins.ts
+++ src/hooks/useFetchCoins.ts
@@ -3,47 +3,21 @@ import { fetchCoinData, fetchCoins } from "../api/coinApi";
 import { Coin } from "../types/type";
 import { getAllBookmarks } from "../utils/localStorage";
 import { useDispatch } from "react-redux";
-import { pushCoins, setCoins, setInitializeFilter } from "../app/globalSlice";
+import { setCoins, setInitializeFilter } from "../app/globalSlice";
 import { toast } from "react-toastify";
+import { useQuery } from "@tanstack/react-query";
 
 export const useFetchCoinList = ({ coins, vsCurrency, perPage, viewMode }: { coins: Coin[]; vsCurrency: string; perPage: number; viewMode: string }) => {
   const dispatch = useDispatch();
-  const CACHE_DURATION = 300000;
-
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
+  const scrapIds = getAllBookmarks();
   const [pageIndex, setPageIndex] = useState(1);
   const [isLastVisible, setIsLastVisible] = useState(true);
 
-  const fetchAndUpdateCoins = async (isCombine: boolean) => {
-    setIsLastVisible(true);
-    setLoading(true);
-
-    const scrapIds = getAllBookmarks();
+  const fetchCoinsData = async () => {
     if (viewMode === "북마크 보기" && scrapIds.length === 0) {
-      dispatch(setCoins([]));
-      return setLoading(false);
+      return [];
     }
 
-    const cacheKey = `coins-${vsCurrency}-${perPage}-${pageIndex}-${viewMode === "북마크 보기" ? scrapIds.join(",") : ""}`;
-    const cachedData = sessionStorage.getItem(cacheKey);
-    console.log(cacheKey);
-
-    if (cachedData) {
-      const { data, timestamp } = JSON.parse(cachedData);
-      if (Date.now() - timestamp < CACHE_DURATION) {
-        if (isCombine) {
-          dispatch(pushCoins(data));
-        } else {
-          setPageIndex(1);
-          dispatch(setCoins(data));
-        }
-        setIsLastVisible(data.length < perPage);
-        setLoading(false);
-        return;
-      }
-    }
     const params = {
       vs_currency: vsCurrency,
       per_page: perPage,
@@ -51,46 +25,53 @@ export const useFetchCoinList = ({ coins, vsCurrency, perPage, viewMode }: { coi
       ...(viewMode === "북마크 보기" && { coinIds: scrapIds }),
     };
 
-    try {
-      const data = await fetchCoins(params);
-      sessionStorage.setItem(cacheKey, JSON.stringify({ data, timestamp: Date.now() }));
-      if (data.length === perPage) {
-        setIsLastVisible(false);
-      }
-      if (isCombine) {
-        dispatch(pushCoins(data));
-      } else {
-        setPageIndex(1);
-        dispatch(setCoins(data));
-      }
-    } catch (error) {
-      dispatch(setCoins([]));
-      toast.error(`요청이 너무 많습니다. 잠시 후 다시 시도해주세요.`, { autoClose: 2000 });
-      setError("error");
-      setLoading(false);
-    } finally {
-      setError(null);
-      setLoading(false);
-    }
+    const data = await fetchCoins(params);
+    return data;
   };
+
+  const { data, error, isLoading, refetch } = useQuery<Coin[], Error>({
+    queryKey: ["coins", vsCurrency, perPage, pageIndex, viewMode],
+    queryFn: fetchCoinsData,
+    staleTime: 300000,
+    refetchInterval: 300000,
+  });
+
+  useEffect(() => {
+    if (data) {
+      if (data.length === 0) {
+        dispatch(setCoins([]));
+      } else {
+        dispatch(setCoins(data));
+        setIsLastVisible(data.length < perPage);
+      }
+    }
+  }, [data, dispatch, perPage]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(`요청이 너무 많습니다. 잠시 후 다시 시도해주세요.`, { autoClose: 2000 });
+    }
+  }, [error]);
+
+  useEffect(() => {
+    dispatch(setInitializeFilter(null));
+  }, [dispatch]);
 
   const handleMoreClick = () => {
-    setPageIndex((prevIndex) => prevIndex + 1);
-    fetchAndUpdateCoins(true);
+    setPageIndex((prevIndex) => {
+      const newIndex = prevIndex + 1;
+      refetch();
+      return newIndex;
+    });
   };
 
-  useEffect(() => {
-    fetchAndUpdateCoins(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [viewMode, vsCurrency, perPage]);
-
-  useEffect(() => {
-    // dispatch(setCoins([]));
-    dispatch(setInitializeFilter(null));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return { coins, loading, error, handleMoreClick, isLastVisible };
+  return {
+    coins,
+    loading: isLoading,
+    error,
+    handleMoreClick,
+    isLastVisible,
+  };
 };
 
 export const useFetchCoin = ({ id }: { id: string | undefined }) => {
